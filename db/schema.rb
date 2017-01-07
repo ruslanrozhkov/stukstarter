f2a2ab1b26d99c2d4ebbde22bbc1411a27b25b2e
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-07 17:21:08 +0200

    Added project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107143519) do
+ActiveRecord::Schema.define(version: 20170107151048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "short_description"
+    t.text     "description"
+    t.string   "image_url"
+    t.string   "status",                                    default: "pending"
+    t.decimal  "goal",              precision: 8, scale: 2
+    t.datetime "expiration_date"
+    t.datetime "created_at",                                                    null: false
+    t.datetime "updated_at",                                                    null: false
+    t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,4 +47,5 @@ ActiveRecord::Schema.define(version: 20170107143519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "projects", "users"
 end
